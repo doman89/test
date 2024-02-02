ef54290096b9f82c512039ec0400decbdd1036b3
Mateusz Domański <mateusz.domanski.1989@gmail.com>
2024-02-02 10:18:31 +0100

    feat: break pipelines

--- src/formatDate/formatDate.js
+++ src/formatDate/formatDate.js
@@ -3,7 +3,7 @@ export function formatDate(date, format = "YYYY-MM-DD") {
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
   const year = date.getFullYear();
 
-  if (format === "DD/MM/YYYY") {
+  if (format !== "DD/MM/YYYY") {
     return `${day}/${month}/${year}`;
   }
 
